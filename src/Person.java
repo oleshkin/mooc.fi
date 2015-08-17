a396613f3597db58420de11e48ca1dad66e6067b
oleshkin <oleshko@tlu.ee>
2015-08-17 18:00:26 +0300

    upd toString: BMI info added.

--- src/Person.java
+++ src/Person.java
@@ -14,7 +14,7 @@ public class Person {
     }
     
     public String toString() {
-        return this.name + ", age " + this.age + " years";
+        return this.name + ", age " + this.age + " years, my body mass index is " + bodyMassIndex();
     }
     
     public void becomeOlder() {
