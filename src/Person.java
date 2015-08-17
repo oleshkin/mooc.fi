583651912459a70ebdc447bb67b3e306c976e9f1
oleshkin <oleshko@tlu.ee>
2015-08-17 17:57:22 +0300

    added weight and height also modifiers/setters for them.

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,16 @@
 public class Person {
+    
     private String name;
     private int age;
+    private int weight;
+    private int height;
+   
     
     public Person(String initialName) {
         this.age = 0;
         this.name = initialName;
+        this.weight = 0;
+        this.height = 0;
     }
     
     public String toString() {
@@ -13,6 +19,14 @@ public class Person {
     
     public void becomeOlder() {
         this.age++;
+    }
+    
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+    
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
     }
     
     public int getAge() {
@@ -25,6 +39,11 @@ public class Person {
     
     public boolean isAdult() {
         return this.age >= 18;
+    }
+    
+    public double bodyMassIndex() {
+        double heightDividedByHundred = this.height / 100;
+        return this.weight / (heightDividedByHundred * heightDividedByHundred);
     }
     
     
