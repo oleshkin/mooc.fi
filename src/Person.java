189851a86fe8b53c1a92d2394755e4adfd3673f9
oleshkin <oleshko@tlu.ee>
2015-08-18 15:22:37 +0300

    Added olderThan method.

--- src/Person.java
+++ src/Person.java
@@ -1,61 +1,69 @@
+
 public class Person {
-    
+
     private String name;
     private int age;
     private int weight;
     private int height;
-   
-    
+
     public Person(String name) {
         this(name, 0);
     }
+
     public Person(String initialName, int age) {
         this.age = age;
         this.name = initialName;
         this.weight = 0;
         this.height = 0;
     }
-    
+
     public String toString() {
         return this.name + ", age " + this.age + " years, my body mass index is " + bodyMassIndex();
     }
-    
+
     public void becomeOlder() {
         this.age++;
     }
-    
+
+    public void becomeOlder(int years) {
+        this.age = this.age + years;
+    }
+
     public void setHeight(int newHeight) {
         this.height = newHeight;
     }
-    
+
     public void setWeight(int newWeight) {
         this.weight = newWeight;
     }
-    
+
     public int getHeight() {
         return this.height;
     }
-    
+
     public int getWeight() {
         return this.weight;
     }
-    
+
     public int getAge() {
         return this.age;
     }
-    
+
     public String getName() {
         return this.name;
     }
-    
+
     public boolean isAdult() {
         return this.age >= 18;
     }
-    
+
     public double bodyMassIndex() {
-        double heightDividedByHundred = this.height / 100;
-        return this.weight / (heightDividedByHundred * heightDividedByHundred);
+        double heightInMeters = this.height / 100.0;
+        return this.weight / (heightInMeters * heightInMeters);
     }
     
-    
+    public boolean olderThan (Person compared) {
+        return this.age > compared.getAge();
+    }
+
 }
